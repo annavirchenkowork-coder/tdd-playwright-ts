1168175156b2441a0a75d17e7d4bebbb564cc05f
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-11-25 07:54:18 -0500

    refactor(review-payment): rewrite page object and reorganize all locators

--- pages/ReviewPaymentPage.ts
+++ pages/ReviewPaymentPage.ts
@@ -1,134 +1,221 @@
-import { FrameLocator, Locator } from "@playwright/test";
+import { Page, FrameLocator, Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class ReviewPaymentPage extends BasePage {
+  public readonly paymentForm: Locator;
+  public readonly paymentFrame: FrameLocator;
 
-  public readonly paymentForm: Locator 
-    = this.locator("//form[@id='payment-form']");
+  public readonly cardNumberInput: Locator;
+  public readonly expiryDateInput: Locator;
+  public readonly cvcInput: Locator;
+  public readonly countryDropDown: Locator;
+  public readonly zipCodeInput: Locator;
 
-  public readonly paymentFrame: FrameLocator 
-    = this.frameLocator("(//iframe[contains(@title, 'Secure payment')])[1]");
+  public readonly byProvidingCardInformationText: Locator;
 
-  public readonly cardNumberInput: Locator 
-    = this.paymentFrame.locator("(//input[@type='text'])[1]");
+  public readonly productPriceText: Locator;
+  public readonly productPriceAmount: Locator;
 
-  public readonly expiryDateInput: Locator 
-    = this.paymentFrame.locator("(//input[@type='text'])[2]");
+  public readonly installmentPriceText: Locator;
+  public readonly installmentPriceAmount: Locator;
 
-  public readonly cvcInput: Locator 
-    = this.paymentFrame.locator("(//input[@type='text'])[3]");
+  public readonly subtotalText: Locator;
+  public readonly subtotalAmount: Locator;
 
-  public readonly countryDropDown: Locator 
-    = this.paymentFrame.locator("//select[@name = 'country']");
+  public readonly processingFeeText: Locator;
+  public readonly processingFeeAmount: Locator;
 
-  public readonly zipCodeInput: Locator 
-    = this.paymentFrame.locator("(//input[@type='text'])[4]");
+  public readonly totalText: Locator;
+  public readonly totalAmount: Locator;
 
-  public readonly byProvidingCardInformationText: Locator 
-    = this.locator("//p[contains(., 'By providing your card information')]");
+  public readonly termsAndConditionsCheckbox: Locator;
+  public readonly termsAndConditionsLink: Locator;
+  public readonly payButton: Locator;
 
-  public readonly productPriceText: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Product Price')]");
+  public readonly cardNumberErrorMessage: Locator;
+  public readonly cardExpiryErrorMessage: Locator;
+  public readonly cardCVCErrorMessage: Locator;
+  public readonly cardZipErrorMessage: Locator;
 
-  public readonly productPriceAmount: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Product Price')]/following-sibling::span");
+  public readonly progressBar: Locator;
+  public readonly backButton: Locator;
+  public readonly footerText: Locator;
 
-  public readonly installmentPriceText: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Installment Price')]");
+  public readonly readAgreeTerms: Locator;
+  public readonly termsAgreementTextPop: Locator;
 
-  public readonly installmentPriceAmount: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Installment Price')]/following-sibling::span");
+  public readonly confirmationBox: Locator;
+  public readonly confirmationBoxSuccess: Locator;
 
-  public readonly subtotalText: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Subtotal')]");
+  public readonly step1Container: Locator;
+  public readonly step2Container: Locator;
+  public readonly step3Container: Locator;
 
-  public readonly subtotalAmount: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Subtotal')]/following-sibling::span");
+  constructor(page: Page) {
+    super(page);
 
-  public readonly processingFeeText: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Processing')]");
+    this.paymentForm = this.locator("//form[@id='payment-form']");
 
-  public readonly processingFeeAmount: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Processing')]/following-sibling::span");
+    this.paymentFrame = this.frameLocator(
+      "(//iframe[contains(@title, 'Secure payment')])[1]"
+    );
 
-  public readonly totalText: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Total')]");
+    this.cardNumberInput = this.paymentFrame.locator(
+      "(//input[@type='text'])[1]"
+    );
 
-  public readonly totalAmount: Locator 
-    = this.locator("//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Total')]/following-sibling::span");
+    this.expiryDateInput = this.paymentFrame.locator(
+      "(//input[@type='text'])[2]"
+    );
 
-  public readonly termsAndConditionsCheckbox: Locator 
-    = this.locator("//input[@type = 'checkbox']");
+    this.cvcInput = this.paymentFrame.locator("(//input[@type='text'])[3]");
 
-  public readonly termsAndConditionsLink: Locator 
-    = this.locator("//u[normalize-space()='Terms and Conditions']");
+    this.countryDropDown = this.paymentFrame.locator(
+      "//select[@name = 'country']"
+    );
 
-  public readonly payButton: Locator 
-    = this.locator("//button[@type='button']");
+    this.zipCodeInput = this.paymentFrame.locator("(//input[@type='text'])[4]");
 
-  public readonly cardNumberErrorMessage: Locator 
-    = this.paymentFrame.locator("//p[@id='Field-numberError' and @class='p-FieldError Error' and @role='alert']");
+    this.byProvidingCardInformationText = this.locator(
+      "//p[contains(., 'By providing your card information')]"
+    );
 
-  public readonly backButton: Locator 
-    = this.locator("(//span[@class='back-button'])[2]");
+    this.productPriceText = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Product Price')]"
+    );
 
-  public readonly footerText: Locator 
-    = this.locator("(//p[@class = 'footer-text' and contains(text(), 'Need help?')])[3]");
+    this.productPriceAmount = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Product Price')]/following-sibling::span"
+    );
 
-  public readonly cardExpiryErrorMessage: Locator 
-    = this.paymentFrame.locator("//p[@id='Field-expiryError' and @class='p-FieldError Error' and @role='alert']");
+    this.installmentPriceText = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Installment Price')]"
+    );
 
-  public readonly cardCVCErrorMessage: Locator 
-    = this.paymentFrame.locator("//p[@id='Field-cvcError' and @class='p-FieldError Error' and @role='alert']");
+    this.installmentPriceAmount = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Installment Price')]/following-sibling::span"
+    );
 
-  public readonly zipCodeErrorMessage: Locator 
-    = this.paymentFrame.locator("//p[@id='Field-postalCodeError' and @class='p-FieldError Error' and @role='alert']");
+    this.subtotalText = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Subtotal')]"
+    );
 
-  public readonly progressBar: Locator 
-    = this.locator("//mat-spinner[@role='progressbar']");
+    this.subtotalAmount = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Subtotal')]/following-sibling::span"
+    );
 
-  public readonly termsAndConditionsCheckBox: Locator 
-    = this.locator("//input[@type='checkbox']");
+    this.processingFeeText = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Processing')]"
+    );
 
-    /*
-    <input _ngcontent-ng-c3758746842="" type="checkbox" id="defaultCheck2" class="form-check-input ng-untouched ng-pristine ng-valid" ng-reflect-model="false">
-    */
+    this.processingFeeAmount = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Processing')]/following-sibling::span"
+    );
 
-  public readonly readAgreeTerms: Locator 
-    = this.locator("//div[3]/div[4]/div[1]/div[2]/div/div[6]");
+    this.totalText = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Total')]"
+    );
 
-  public readonly termsAgreementTextPop: Locator 
-    = this.locator("//h1[@id='mat-mdc-dialog-title-0']");
+    this.totalAmount = this.locator(
+      "//div[@class='content-panel-item ng-star-inserted']/span[contains(text(), 'Total')]/following-sibling::span"
+    );
 
-  public async enterCardNumber(cardNumber: string = process.env.CARD_NUMBER as string) {
+    this.termsAndConditionsCheckbox = this.locator(
+      "//input[@type = 'checkbox']"
+    );
+
+    this.termsAndConditionsLink = this.locator(
+      "//u[normalize-space()='Terms and Conditions']"
+    );
+
+    this.payButton = this.locator("//button[@type='button']");
+
+    this.cardNumberErrorMessage = this.paymentFrame.locator(
+      "//p[@id='Field-numberError' and @role='alert']"
+    );
+
+    this.cardExpiryErrorMessage = this.paymentFrame.locator(
+      "//p[@id='Field-expiryError' and @class='p-FieldError Error' and @role='alert']"
+    );
+
+    this.cardCVCErrorMessage = this.paymentFrame.locator(
+      "//p[@id='Field-cvcError' and @class='p-FieldError Error' and @role='alert']"
+    );
+
+    this.cardZipErrorMessage = this.paymentFrame.locator(
+      "//p[@id='Field-postalCodeError' and @class='p-FieldError Error' and @role='alert']"
+    );
+
+    this.progressBar = this.locator("//mat-spinner[@role='progressbar']");
+
+    this.backButton = this.locator("(//span[@class='back-button'])[2]");
+
+    this.footerText = this.locator(
+      "(//p[@class = 'footer-text' and contains(text(), 'Need help?')])[3]"
+    );
+
+    this.readAgreeTerms = this.locator(
+      "//div[3]/div[4]/div[1]/div[2]/div/div[6]"
+    );
+
+    this.termsAgreementTextPop = this.locator(
+      "//h1[@id='mat-mdc-dialog-title-0']"
+    );
+
+    this.confirmationBox = this.locator(
+      "//div[contains(., 'Payments confirmation')][contains(@class,'ng-star-inserted')]"
+    );
+
+    this.confirmationBoxSuccess = this.locator(
+      "//div[contains(@class,'success-container') or contains(., 'Payment successful')]"
+    );
+
+    this.step1Container = this.locator(
+      "//div[contains(concat(' ', normalize-space(@class), ' '), ' step ')][.//div[contains(@class,'step-circle')]/span[normalize-space()='1']]"
+    );
+
+    this.step2Container = this.locator(
+      "//div[contains(concat(' ', normalize-space(@class), ' '), ' step ')][.//div[contains(@class,'step-circle')]/span[normalize-space()='2']]"
+    );
+
+    this.step3Container = this.locator(
+      "//div[contains(concat(' ', normalize-space(@class), ' '), ' step ')][.//div[contains(@class,'step-circle')]/span[normalize-space()='3']]"
+    );
+  }
+
+  public async enterCardNumber(
+    cardNumber: string = process.env.CARD_NUMBER as string
+  ): Promise<void> {
     await this.cardNumberInput.fill(cardNumber);
   }
 
-  public async enterExpiryDate(expiryDate: string = process.env.CARD_EXPIRATION_DATE as string) {
+  public async enterExpiryDate(
+    expiryDate: string = process.env.CARD_EXPIRATION_DATE as string
+  ): Promise<void> {
     await this.expiryDateInput.fill(expiryDate);
   }
 
-  public async enterCVC(cvc: string = process.env.CARD_SECURITY_CODE as string) {
+  public async enterCVC(
+    cvc: string = process.env.CARD_SECURITY_CODE as string
+  ): Promise<void> {
     await this.cvcInput.fill(cvc);
   }
 
-  public async enterZipCode(zipCode: string = process.env.ZIP_CODE as string) {
+  public async enterZipCode(
+    zipCode: string = process.env.ZIP_CODE as string
+  ): Promise<void> {
     await this.zipCodeInput.fill(zipCode);
   }
 
-  public async clickTermsAndConditionsCheckbox() {
+  public async clickTermsAndConditionsCheckbox(): Promise<void> {
     await this.termsAndConditionsCheckbox.click();
   }
 
-  public async clickBackButton() {
+  public async clickBackButton(): Promise<void> {
     await this.backButton.click();
   }
 
-  public async clickPayButton() {
+  public async clickPayButton(): Promise<void> {
     await this.payButton.click();
   }
-
-
-
-
 }
